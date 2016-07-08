a4e10ba52ac812dea6cbcff0c4805db7b88e9711
Ahmad Amireh <ahmad@amireh.net>
2016-07-09 02:18:19 +0300

    Update license in headers

--- src/delta_encoder.cpp
+++ src/delta_encoder.cpp
@@ -1,3 +1,23 @@
+/**
+ * karazeh -- the library for patching software
+ *
+ * Copyright (C) 2011-2016 by Ahmad Amireh <ahmad@amireh.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include "karazeh/delta_encoder.hpp"
 
 namespace kzh {
