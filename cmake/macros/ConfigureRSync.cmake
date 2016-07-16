367799f10189e3c098d858cdbc4ed45e9479f655
Ahmad Amireh <ahmad@amireh.net>
2016-07-16 07:26:56 +0300

    Update docs

--- cmake/macros/ConfigureRSync.cmake
+++
@@ -1,9 +0,0 @@
-# librsync-2.0.0
-INCLUDE_DIRECTORIES(${CMAKE_BINARY_DIR}/deps/librsync-2.0.0/src)
-
-# librsync-0.9.7
-# IF(WIN32)
-#   INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/deps/librsync-0.9.7/windows)
-# ELSE(UNIX)
-#   INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/deps/librsync-0.9.7/linux)
-# ENDIF()
--- test/fixture/sample_application/0.1.1/src/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char** argv) {
-	printf("I'm version 0.1.1\n");
-	return 0;
-}
